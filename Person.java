a905527314184657556fbec81534768618bcfe82
buzzed1 <smitstephen@gmail.com>
2017-01-24 15:12:11 -0500

    Bus Scheduler
    This app keeps track of passenger data and determines how many buses are
    need based on the number travelling. It also calculates the total fare.

---
+++ Person.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bus_scheduler;
+
+/**
+ *
+ * @author Steve
+ */
+public class Person {
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String address;
+    private int age;
+    private int cellphone;
+    private int homephone;
+    
+}
